d27530270052ee8873fa0ac571d9f4acac6a7de9
Rex Tsou <akccakccwww@gmail.com>
2017-07-19 11:55:57 +0800

    Update styles

---
+++ dist/js/sec-generation.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var swiperConfig1 = {
+  pagination: '.swiper-pagination',
+  slidesPerView: 1,
+  paginationClickable: true,
+  spaceBetween: 30,
+  grabcursor: true
+};
+
+var swiperConfig2 = {
+  pagination: '.swiper-pagination',
+  slidesPerView: 3,
+  paginationClickable: true,
+  spaceBetween: 30,
+  grabcursor: true
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  if (window.innerWidth < 900) {
+    var swiper = new Swiper('.swiper-container', swiperConfig1);
+    console.log('teste1');
+  } else {
+    var _swiper = new Swiper('.swiper-container', swiperConfig2);
+    console.log('hey apple');
+  }
+});
---
+++ src/js/sec-generation.js
@@ -0,0 +1,25 @@
+const swiperConfig1 = {
+  pagination: '.swiper-pagination',
+  slidesPerView: 1,
+  paginationClickable: true,
+  spaceBetween: 30,
+  grabcursor: true,
+};
+
+const swiperConfig2 = {
+  pagination: '.swiper-pagination',
+  slidesPerView: 1,
+  paginationClickable: true,
+  spaceBetween: 30,
+  grabcursor: true,
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (window.innerWidth < 900) {
+    const swiper = new Swiper('.swiper-container', swiperConfig1);
+    console.log('teste1');
+  } else {
+    const swiper = new Swiper('.swiper-container', swiperConfig2);
+    console.log('hey apple');
+  }
+});
