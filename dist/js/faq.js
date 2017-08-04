548b40208221aaabc0a583d7d5893c659173059d
Rex Tsou <akccakccwww@gmail.com>
2017-08-04 18:56:46 +0800

    Add page FAQ and basic style

---
+++ dist/js/faq.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var closeFAQs = function closeFAQs() {
+  var faqTypeContainers = document.querySelectorAll('.faq-type-container');
+  faqTypeContainers.forEach(function (item) {
+    item.classList.remove('is-active');
+  });
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  var faq = document.querySelector('#faq');
+  var faqBricks = faq.querySelectorAll('.brick');
+  var faqTitles = faq.querySelectorAll('.faq-group .title-container');
+
+  faqBricks.forEach(function (brick) {
+    brick.addEventListener('click', function (e) {
+      var targetType = e.currentTarget.dataset.type;
+      var targetEl = document.querySelector('#faq-' + targetType);
+      closeFAQs();
+      targetEl.classList.toggle('is-active');
+    });
+  });
+
+  faqTitles.forEach(function (title) {
+    title.addEventListener('click', function (e) {
+      var targetContent = e.currentTarget.nextElementSibling || e.currentTarget.nextSibling;
+      targetContent.classList.toggle('is-active');
+    });
+  });
+});
---
+++ src/js/faq.js
@@ -0,0 +1,28 @@
+const closeFAQs = () => {
+  const faqTypeContainers = document.querySelectorAll('.faq-type-container');
+  faqTypeContainers.forEach((item) => {
+    item.classList.remove('is-active');
+  });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const faq = document.querySelector('#faq');
+  const faqBricks = faq.querySelectorAll('.brick');
+  const faqTitles = faq.querySelectorAll('.faq-group .title-container');
+
+  faqBricks.forEach((brick) => {
+    brick.addEventListener('click', (e) => {
+      const targetType = e.currentTarget.dataset.type;
+      const targetEl = document.querySelector(`#faq-${targetType}`);
+      closeFAQs();
+      targetEl.classList.toggle('is-active');
+    });
+  });
+
+  faqTitles.forEach((title) => {
+    title.addEventListener('click', (e) => {
+      const targetContent = e.currentTarget.nextElementSibling || e.currentTarget.nextSibling;
+      targetContent.classList.toggle('is-active');
+    });
+  });
+});
