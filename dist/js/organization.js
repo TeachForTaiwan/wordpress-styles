7a48fe15564357476daa5f74e1978839e67a6c2e
akccakcctw <akccakccwww@gmail.com>
2017-08-17 18:51:00 +0800

    Update organization.js

--- dist/js/organization.js
+++ dist/js/organization.js
@@ -1,9 +1,17 @@
 'use strict';
 
+var rmClass = function rmClass(el, cName) {
+  el.classList.remove(cName);
+};
+
 var closeTabs = function closeTabs() {
   var tabs = document.querySelectorAll('.org-container .org-content');
+  var orgItems = document.querySelectorAll('.org-container .item');
   [].forEach.call(tabs, function (tab) {
-    tab.classList.remove('is-active');
+    rmClass(tab, 'is-active');
+  });
+  [].forEach.call(orgItems, function (item) {
+    rmClass(item, 'is-active');
   });
 };
 
@@ -12,11 +20,15 @@ document.addEventListener('DOMContentLoaded', function () {
   var orgTabs = org.querySelectorAll('.item');
   [].forEach.call(orgTabs, function (tab) {
     tab.addEventListener('click', function (e) {
+      if (e.currentTarget.classList.contains('is-active')) {
+        closeTabs();
+        return;
+      }
       var targetId = e.currentTarget.dataset.id;
       var targetEl = document.getElementById(targetId);
       closeTabs();
+      e.currentTarget.classList.add('is-active');
       targetEl.classList.add('is-active');
-      console.log(targetEl);
     });
   });
 });
--- src/js/organization.js
+++ src/js/organization.js
@@ -1,7 +1,15 @@
+const rmClass = (el, cName) => {
+  el.classList.remove(cName);
+};
+
 const closeTabs = () => {
   const tabs = document.querySelectorAll('.org-container .org-content');
+  const orgItems = document.querySelectorAll('.org-container .item');
   [].forEach.call(tabs, (tab) => {
-    tab.classList.remove('is-active');
+    rmClass(tab, 'is-active');
+  });
+  [].forEach.call(orgItems, (item) => {
+    rmClass(item, 'is-active');
   });
 };
 
@@ -10,11 +18,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const orgTabs = org.querySelectorAll('.item');
   [].forEach.call(orgTabs, (tab) => {
     tab.addEventListener('click', (e) => {
+      if (e.currentTarget.classList.contains('is-active')) {
+        closeTabs();
+        return;
+      }
       const targetId = e.currentTarget.dataset.id;
       const targetEl = document.getElementById(targetId);
       closeTabs();
+      e.currentTarget.classList.add('is-active');
       targetEl.classList.add('is-active');
-      console.log(targetEl);
     });
   });
 });
