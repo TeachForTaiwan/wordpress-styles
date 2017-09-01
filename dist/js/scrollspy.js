e872320a4b8b43acd038e3c8e10abe66df91d763
Rex Tsou <akccakccwww@gmail.com>
2017-09-01 16:15:46 +0800

    Fix scrollspy bug

--- dist/js/scrollspy.js
+++ dist/js/scrollspy.js
@@ -1,44 +1,55 @@
 'use strict';
 
 /* global jQuery */
-var scrollSpy = document.querySelector('.scrollspy');
-var scrollSpyLink = scrollSpy.querySelectorAll('.link');
-var removeActiveClass = function removeActiveClass() {
-  scrollSpyLink.forEach(function (el) {
-    el.parentNode.classList.remove('is-active', 'is-current');
-  });
-};
-
-window.addEventListener('load', function () {
+document.addEventListener('DOMContentLoaded', function () {
+  // query elems
+  var scrollSpy = document.querySelector('.scrollspy');
+  var scrollSpyLink = scrollSpy.querySelectorAll('.link');
   var spyKey = document.querySelector('.scrollspy-container').dataset.key;
   var spySections = document.querySelectorAll('.' + spyKey);
   var spySection = {};
-  var spyElementOffsetBottom = document.body.scrollHeight - 1000;
-  spySections.forEach(function (e) {
-    spySection[e.id] = e.offsetTop;
-  });
-  var spyElementOffsetTop = spySection[spyKey + '-1']; // first-section of scrollspy
+  var spyElementOffsetTop = void 0; // first-section of scrollspy
+  var spyElementOffsetBottom = void 0; // last-section of scrollspy
 
-  window.addEventListener('scroll', function () {
-    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
-    if (scrollPosition > spyElementOffsetTop && scrollPosition < spyElementOffsetBottom) {
-      document.querySelector('.scrollspy').classList.add('is-show');
-    } else {
-      document.querySelector('.scrollspy').classList.remove('is-show');
-    }
-
-    jQuery.each(spySection, function (k, value) {
-      if (value <= scrollPosition + 150) {
-        removeActiveClass();
-        document.querySelector('.scrollspy .link[href*="' + k + '"]').parentNode.classList.add('is-active');
-      }
+  // fns
+  var removeActiveClass = function removeActiveClass() {
+    scrollSpyLink.forEach(function (el) {
+      el.parentNode.classList.remove('is-active', 'is-current');
     });
-  });
-});
+  };
+  var updateSectionOffset = function updateSectionOffset() {
+    spySections.forEach(function (sec) {
+      spySection[sec.id] = sec.offsetTop;
+    });
+    spyElementOffsetTop = spySection[spyKey + '-1'];
+    spyElementOffsetBottom = document.body.scrollHeight - 1000;
+  };
 
-document.addEventListener('DOMContentLoaded', function () {
+  // event listener
+  window.addEventListener('load', function () {
+    updateSectionOffset();
+
+    window.addEventListener('scroll', function () {
+      updateSectionOffset();
+      var scrollPosition = document.body.scrollTop || document.documentElement.scrollTop;
+      if (scrollPosition > spyElementOffsetTop && scrollPosition < spyElementOffsetBottom) {
+        document.querySelector('.scrollspy').classList.add('is-show');
+      } else {
+        document.querySelector('.scrollspy').classList.remove('is-show');
+      }
+
+      jQuery.each(spySection, function (k, value) {
+        if (value <= scrollPosition + 150) {
+          removeActiveClass();
+          document.querySelector('.scrollspy .link[href*="' + k + '"]').parentNode.classList.add('is-active');
+        }
+      });
+    });
+    window.addEventListener('resize', updateSectionOffset);
+  });
+
   scrollSpyLink.forEach(function (link) {
-    link.addEventListener('click', function (e) {
+    return link.addEventListener('click', function (e) {
       var elemHref = e.currentTarget.getAttribute('href');
       var elem = document.querySelector(elemHref);
       var curIndex = Array.from(elem.parentNode.children).indexOf(elem);
--- src/js/scrollspy.js
+++ src/js/scrollspy.js
@@ -1,52 +1,61 @@
 /* global jQuery */
-const scrollSpy = document.querySelector('.scrollspy');
-const scrollSpyLink = scrollSpy.querySelectorAll('.link');
-const removeActiveClass = () => {
-  scrollSpyLink.forEach((el) => {
-    el.parentNode.classList.remove('is-active', 'is-current');
-  });
-};
-
-window.addEventListener('load', () => {
+document.addEventListener('DOMContentLoaded', () => {
+  // query elems
+  const scrollSpy = document.querySelector('.scrollspy');
+  const scrollSpyLink = scrollSpy.querySelectorAll('.link');
   const spyKey = document.querySelector('.scrollspy-container').dataset.key;
   const spySections = document.querySelectorAll(`.${spyKey}`);
   const spySection = {};
-  const spyElementOffsetBottom = document.body.scrollHeight - 1000;
-  spySections.forEach((e) => {
-    spySection[e.id] = e.offsetTop;
-  });
-  const spyElementOffsetTop = spySection[`${spyKey}-1`]; // first-section of scrollspy
+  let spyElementOffsetTop; // first-section of scrollspy
+  let spyElementOffsetBottom; // last-section of scrollspy
 
-  window.addEventListener('scroll', () => {
-    const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
-    if (scrollPosition > spyElementOffsetTop && scrollPosition < spyElementOffsetBottom) {
-      document.querySelector('.scrollspy').classList.add('is-show');
-    } else {
-      document.querySelector('.scrollspy').classList.remove('is-show');
-    }
+  // fns
+  const removeActiveClass = () => {
+    scrollSpyLink.forEach((el) => {
+      el.parentNode.classList.remove('is-active', 'is-current');
+    });
+  };
+  const updateSectionOffset = () => {
+    spySections.forEach((sec) => {
+      spySection[sec.id] = sec.offsetTop;
+    });
+    spyElementOffsetTop = spySection[`${spyKey}-1`];
+    spyElementOffsetBottom = document.body.scrollHeight - 1000;
+  };
 
-    jQuery.each(spySection, (k, value) => {
-      if (value <= scrollPosition + 150) {
-        removeActiveClass();
-        document.querySelector(`.scrollspy .link[href*="${k}"]`).parentNode.classList.add('is-active');
+  // event listener
+  window.addEventListener('load', () => {
+    updateSectionOffset();
+
+    window.addEventListener('scroll', () => {
+      updateSectionOffset();
+      const scrollPosition = document.body.scrollTop || document.documentElement.scrollTop;
+      if (scrollPosition > spyElementOffsetTop && scrollPosition < spyElementOffsetBottom) {
+        document.querySelector('.scrollspy').classList.add('is-show');
+      } else {
+        document.querySelector('.scrollspy').classList.remove('is-show');
       }
-    });
-  });
-});
 
-document.addEventListener('DOMContentLoaded', () => {
-  scrollSpyLink.forEach((link) => {
-    link.addEventListener('click', (e) => {
-      const elemHref = e.currentTarget.getAttribute('href');
-      const elem = document.querySelector(elemHref);
-      const curIndex = Array.from(elem.parentNode.children).indexOf(elem);
-
-      // scroll anim
-      e.preventDefault();
-      jQuery('html,body').animate({ scrollTop: jQuery(elemHref).offset().top - 60 }, 800);
-      removeActiveClass();
-      scrollSpyLink[curIndex].parentNode.classList.add('is-active');
-      elem.classList.add('is-current');
+      jQuery.each(spySection, (k, value) => {
+        if (value <= scrollPosition + 150) {
+          removeActiveClass();
+          document.querySelector(`.scrollspy .link[href*="${k}"]`).parentNode.classList.add('is-active');
+        }
+      });
     });
+    window.addEventListener('resize', updateSectionOffset);
   });
+
+  scrollSpyLink.forEach(link => link.addEventListener('click', (e) => {
+    const elemHref = e.currentTarget.getAttribute('href');
+    const elem = document.querySelector(elemHref);
+    const curIndex = Array.from(elem.parentNode.children).indexOf(elem);
+
+    // scroll anim
+    e.preventDefault();
+    jQuery('html,body').animate({ scrollTop: jQuery(elemHref).offset().top - 60 }, 800);
+    removeActiveClass();
+    scrollSpyLink[curIndex].parentNode.classList.add('is-active');
+    elem.classList.add('is-current');
+  }));
 });
