743b2bc70ee471470466ada1b77ae82bc853dcb5
Rex Tsou <akccakccwww@gmail.com>
2017-11-02 10:50:23 +0800

    Update sponser module

--- dist/module/sponser/sponser.js
+++ dist/module/sponser/sponser.js
@@ -15,18 +15,50 @@ document.addEventListener('DOMContentLoaded', function () {
     return d.list;
   }).enter().append('figure');
 
-  var img = function img() {
-    item.filter(function (d) {
-      return d.logo;
-    }).append('div').classed('partner-img', true).append('a').attr('href', function (d) {
-      return d.url ? d.url : 'javascript:;';
-    }).attr('target', '_blank').attr('rel', 'noopener').append('img').attr('src', function (d) {
-      return d.logo;
-    });
-  };
-  img();
+  var img = item.filter(function (d) {
+    return d.logo;
+  }).append('div').classed('partner-img', true).append('a').attr('href', function (d) {
+    return d.url ? d.url : 'javascript:;';
+  }).attr('target', '_blank').attr('rel', 'noopener').append('img').attr('src', function (d) {
+    return d.logo;
+  }).attr('alt', function (d) {
+    return d.name;
+  });
 
   var name = item.append('figcaption').classed('partner-name', true).text(function (d) {
     return d.name;
+  });
+
+  var intro = item.filter(function (d) {
+    return d.intro;
+  }).append('div').classed('intro', true).selectAll('p').data(function (d) {
+    return d.intro;
+  }).enter().append('p').text(function (d) {
+    return d;
+  });
+
+  // intro event
+  var sponsers = document.querySelectorAll('.partner-listing figure');
+  var introContent = document.querySelector('.intro-content');
+  var hasIntro = function hasIntro(el) {
+    return !!el.querySelector('.intro');
+  };
+  var showIntro = function showIntro(el) {
+    if (!hasIntro(el)) return false;
+    el.classList.add('intro-show');
+  };
+  var fadeIntro = function fadeIntro(el) {
+    if (!hasIntro(el)) return false;
+    el.classList.remove('intro-show');
+  };
+  [].forEach.call(sponsers, function (item) {
+    item.addEventListener('mouseenter', function (e) {
+      var target = e.currentTarget;
+      showIntro(target);
+    });
+    item.addEventListener('mouseleave', function (e) {
+      var target = e.currentTarget;
+      fadeIntro(target);
+    });
   });
 });
--- src/module/sponser/sponser.js
+++ src/module/sponser/sponser.js
@@ -26,25 +26,58 @@ document.addEventListener('DOMContentLoaded', () => {
     .enter()
     .append('figure')
 
-  const img = () => {
-    item
-      .filter(d => d.logo)
-      .append('div')
-      .classed('partner-img', true)
-      .append('a')
-      .attr('href', (d) => {
-        return d.url ? d.url : 'javascript:;'
-      })
-      .attr('target', '_blank')
-      .attr('rel', 'noopener')
-      .append('img')
-      .attr('src', d => d.logo)
-  };
-  img();
+  const img = item
+    .filter(d => d.logo)
+    .append('div')
+    .classed('partner-img', true)
+    .append('a')
+    .attr('href', (d) => {
+      return d.url ? d.url : 'javascript:;'
+    })
+    .attr('target', '_blank')
+    .attr('rel', 'noopener')
+    .append('img')
+    .attr('src', d => d.logo)
+    .attr('alt', d => d.name)
 
   const name = item
     .append('figcaption')
     .classed('partner-name', true)
     .text(d => d.name)
 
+  const intro = item
+    .filter(d => d.intro)
+    .append('div')
+    .classed('intro', true)
+    .selectAll('p')
+    .data(d => d.intro)
+    .enter()
+    .append('p')
+    .text(d => d)
+
+  // intro event
+  const sponsers = document.querySelectorAll('.partner-listing figure');
+  const introContent = document.querySelector('.intro-content');
+  const hasIntro = (el) => {
+    return !!el.querySelector('.intro');
+  };
+  const showIntro = (el) => {
+    if (!hasIntro(el)) return false;
+    el.classList.add('intro-show');
+  };
+  const fadeIntro = (el) => {
+    if (!hasIntro(el)) return false;
+    el.classList.remove('intro-show');
+  }
+  [].forEach.call(sponsers, (item) => {
+    item.addEventListener('mouseenter', (e) => {
+      const target = e.currentTarget;
+      showIntro(target);
+    });
+    item.addEventListener('mouseleave', (e) => {
+      const target = e.currentTarget;
+      fadeIntro(target);
+    })
+  });
 });
+
